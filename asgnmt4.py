a971f7513f0b6b238a54f1ba23bc506b006a1f75
SACHIN SHARMA <104039544+98sachin@users.noreply.github.com>
2024-05-18 23:20:26 +0530

    Add files via upload

---
+++ asgnmt4.py
@@ -0,0 +1,80 @@
+"""
+Assignment - 4 Full Stack Web Development using Python MySirG
+User Input Problems
+
+1. Write a python script to take your name as input from the user and then print it.
+2. Write a python script to take input from the user. Input must be a number.
+3. Write a python script which takes two numbers from the user, then calculate their sum
+and display the result.
+4. Write a python script which takes the radius from the user and display area of a circle.
+5. Write a python script to calculate the square of a number. Number is entered by the user.
+6. Write a python script to calculate the area of Triangle. Number is entered by the user.
+7. Write a python script to calculate average of three numbers, entered by the user
+8. Write a python script to calculate simple interest
+9. Write a python script to calculate the volume of a cuboid.
+10. Write a python script to calculate area of a rectangle.
+"""
+
+# 1. Write a python script to take your name as input from the user and then print it.
+
+# a=input("Enter the name: ");
+# print(a);
+
+# ===========================================================================================
+
+# if you press shift+enter it will open python terminal and show its version. Try pressing ctrl+v/ctrl+V in cmd.
+
+# ===========================================================================================
+
+# 2. Write a python script to take input from the user. Input must be a number.
+
+# a=int(input("Enter a number: "));
+# print(a);
+
+# ===========================================================================================
+
+# 3. Write a python script which takes two numbers from the user, then calculate their sum and display the result.
+
+# a=int(input("Enter the 1st no.: "));
+# b=int(input("Enter the 2nd no.: "));
+# c=a+b;
+# print(c);
+
+# ==========================================================================================
+
+# 4. Write a python script which takes the radius from the user and display area of a circle.
+
+# a=float(input("Enter the radius: "));
+# b=3.14;
+# c=b*a**2; # b*a*a
+# print(c);
+
+# ==========================================================================================
+
+# 5. Write a python script to calculate the square of a number. Number is entered by the user.
+# a=int(input("Enter a number: "));
+# print(a**2);
+
+# ==========================================================================================
+
+# 6. Write a python script to calculate the area of Triangle. Number is entered by the user.
+
+# a=float(input("Enter the base: "));
+# b=float(input("Enter the height: "));
+# c=0.5*a*b;
+# print(c);
+
+# ==========================================================================================
+
+# 7. Write a python script to calculate average of three numbers, entered by the user
+
+# a=float(input("Enter the 1st number: "));
+# b=float(input("Enter the 2nd number: "));
+# c=float(input("Enter the 3rd number: "));
+# d=(a+b+c)/3;
+# print(d);
+
+# ===========================================================================================
+
+# 8. Write a python script to calculate simple interest
+
