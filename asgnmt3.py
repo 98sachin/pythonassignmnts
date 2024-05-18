30c8d7147e68c470a06e3d899ac20d514c161513
SACHIN SHARMA <104039544+98sachin@users.noreply.github.com>
2024-05-18 23:19:25 +0530

    Add files via upload

---
+++ asgnmt3.py
@@ -0,0 +1,91 @@
+"""
+Assignment-3: Type Conversion
+1. Write a python script to convert a number into str type.
+2. Write a python script to print Unicode of the character ‘m’
+3. Write a python script to print character representation of a given unicode 100.
+4. Write a python script to print any number and its binary equivalent
+5. Write a python script to print any number and its octal equivalent.
+6. Write a python script to print any number and its hexadecimal equivalent.
+7. Write a python script to store binary number 1100101 in a variable and print it in
+decimal format.
+8. Write a python script to store a hexadecimal number 2F in a variable and print it in
+octal format.
+9. Write a python script to store an octal number 125 in a variable and print it in binary
+format.
+10. Write a python script to add two numbers 25 (in octal) and 39 (in hexadecimal) and
+display the result in binary format.
+"""
+
+# 1. Write a python script to convert a number into str type.
+# a=5;
+# b=str(a);
+# print(type(b));
+
+# =================================================================================
+
+# 2. Write a python script to print Unicode of the character ‘m’.
+# a='m';
+# print(ord('m'));
+
+# ==================================================================================
+
+# 3. Write a python script to print character representation of a given unicode 100.
+# a=100;
+# print(chr(100));
+
+# ===================================================================================
+
+# 4. Write a python script to print any number and its binary equivalent.
+# a=23;
+# print(bin(a));
+
+# ===================================================================================
+
+# 5. Write a python script to print any number and its octal equivalent.
+# a=23;
+# print(oct(a));
+
+# ===================================================================================
+
+# 6. Write a python script to print any number and its hexadecimal equivalent.
+# a=23;
+# print(hex(a));
+
+# ===================================================================================
+# IMPORTANT
+# 7. Write a python script to store binary number 1100101 in a variable and print it in decimal format.
+    # a='0b1100101';
+    # b=int(a,2);
+    # print(b);
+
+    # OR
+    # print(oct(a));
+# ===================================================================================
+
+# 8. Write a python script to store a hexadecimal number 2F in a variable and print it in octal format.
+    # a='0x2F';
+    # b=int(a,16);
+    # print(b);
+    #   OR
+    # print(oct(a));
+# ===================================================================================
+
+# 9. Write a python script to store an octal number 125 in a variable and print it in binary format.
+    # a='0o125'
+    # b=int(a,8);
+    # print(b);
+    #   OR
+    # print(bin(a));
+
+# ===================================================================================
+# 10. Write a python script to add two numbers 25 (in octal) and 39 (in hexadecimal) and display the result in binary format.
+    # a='0o25';
+    # b='0x39';
+    # c=int(a,8)+int(b,16);
+    # print(c);
+    #    OR
+    # print(bin(a)+bin(b));
+    #   OR
+    # print(bin(0o25)+bin(0x39));
+
+# ===================================================================================
