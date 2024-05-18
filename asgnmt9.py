9769ced4f6401482764e430b6660dd26d7b3d131
SACHIN SHARMA <104039544+98sachin@users.noreply.github.com>
2024-05-18 23:25:50 +0530

    Add files via upload

---
+++ asgnmt9.py
@@ -0,0 +1,115 @@
+"""
+Assignment - 9 Full Stack Web Development using Python MySirG
+While Loop II
+1. Write a python script to print MySirG N times on the screen
+2. Write a python script to print first N natural numbers
+3. Write a python script to print first N natural numbers in reverse order
+4. Write a python script to print first N odd natural numbers
+5. Write a python script to print first N odd natural numbers in reverse order
+6. Write a python script to print first N even natural numbers
+7. Write a python script to print first N even natural numbers in reverse order
+8. Write a python script to print squares of first N natural numbers
+9. Write a python script to print cubes of first N natural numbers
+10. Write a python script to print first 10 multiples of N
+"""
+
+# Q1. Write a python script to print MySirG N times on the screen.
+
+# n=int(input('Enter a number:'))
+# i=1;
+# while i<=n:
+#     print('MySirG')
+#     i+=1;
+# print()
+# ===========================================================================
+
+# Q2. Write a python script to print first N natural numbers with MySirG.
+
+# n=int(input('Enter a number:'))
+# i=1;
+# while i<=n:
+#     print(i,'MySirG')
+#     i+=1;
+# print()
+# ===========================================================================
+
+# Writing the same above code without using i variable
+
+# n=int(input('Enter a number:'))
+# while n:
+#     print('MySirG')
+#     n-=1;
+# print()
+# ===========================================================================
+
+# Q3. Write a python script to print first N natural numbers in reverse order.
+
+# n= int(input('Enter your number: '));
+
+# while n>0:
+#     print(n,end=' ');
+#     n-=1;
+# =============================================================================
+
+# Q4. Write a python script to print first N odd natural numbers.
+
+# n=int(input('Enter a number: '))
+# i=1;
+# while i<n*2:
+#     print(i);
+#     i+=2;
+# =============================================================================
+
+# Q5. Write a python script to print first N odd natural numbers in reverse order.
+
+# n=int(input("Enter a number: "))
+# i=n*2-1;
+# while i>=1:
+#     print(i);
+#     i-=2;
+# ==============================================================================
+
+# Q6. Write a python script to print first N even natural numbers.
+
+# n= int(input('Enter a number: '))
+# i=2;
+# while i<=2*n:
+#     print(i);
+#     i+=2;
+# ==============================================================================
+
+# Q7. Write a python script to print first N even natural numbers in reverse order.
+
+# n= int(input("Enter a number: "));
+# i=n*2;
+# while i>=2:
+#     print(i);
+#     i-=2;
+# ==============================================================================
+
+# Q8. Write a python script to print squares of first N natural numbers.
+
+# n=int(input("Enter a number: "));
+# i=1;
+# while n>=i:
+#     print(i**2)
+#     i+=1;
+# ===============================================================================
+
+# Q9. Write a python script to print cubes of first N natural numbers.
+
+# n=int(input("Enter a number: "));
+# i=1;
+# while n>=i:
+#     print(i**3)
+#     i+=1;
+# ===============================================================================
+
+# Q10. Write a python script to print first 10 multiples of N.
+
+# n=int(input("Enter a number: "));
+# i=1;
+# while i<=10:
+#     print(n*i);
+#     i+=1;
+# ===============================================================================
